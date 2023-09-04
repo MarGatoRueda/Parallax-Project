9ed9624bf1c32b9876108d635d3b6b4054119e32
Rho02 <rodrigo.mendoza@ug.uchile.cl>
2023-09-04 12:42:03 -0300

    pause

--- Menu/pause.gd
+++ Menu/pause.gd
@@ -21,14 +21,17 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		visible = !visible
 		get_tree().paused = visible
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_resume_pressed():
 	hide()
-	
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	
 	
 func _on_retry_pressed():
 	get_tree().reload_current_scene()
+	get_tree().paused = false
 	
 	
 	
@@ -36,6 +39,7 @@ func _on_menu_pressed():
 	if not main_menu:
 		return
 	get_tree().change_scene_to_packed(main_menu)
+	get_tree().paused = false
 	
 	
 	
