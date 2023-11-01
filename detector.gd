8ee018204e4db9f9ceb0c0f2631eb00c5fc10fb0
NepJag <dylan.riquelme@ug.uchile.cl>
2023-11-01 19:01:04 -0300

    Re-implemented the teleportation with raycast

--- detector.gd
+++ detector.gd
@@ -1,13 +1,16 @@
 extends Area3D
 
 var player : Player = null
+var raycast : RayCast3D = null
 var rotation_threshold = 0.0005
-@onready var local_rotation = $LocalRotation
+@onready var local_rotation : Marker3D = $LocalRotation
+@onready var target : Marker3D = $"../TargetArea/Target"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(false)
 	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exit)
 	pass # Replace with function body.
 
 
@@ -19,21 +22,28 @@ func _process(delta):
 func _physics_process(delta):
 	if not is_instance_valid(player):
 		return
-	var camera_rotation = player.camera.quaternion
-	var raycast : RayCast3D = player.ray_detector
-	raycast.is_colliding()
-	var diff = local_rotation.quaternion.dot(camera_rotation)
-	print(abs(1-diff))
+	var distance = local_rotation.global_position.distance_to(target.global_position)
+	raycast.target_position = Vector3(0, distance*1.2, 0)
+	
+	if raycast.is_colliding():
+		# Teleport
+		player.global_position = target.global_position
+	#var camera_rotation = player.camera.quaternion
+	#var diff = local_rotation.quaternion.dot(camera_rotation)
+	#print(abs(1-diff))
 
 func _on_body_entered(body: Node3D):
 	if not body is Player:
 		return
 	player = body
+	raycast = player.ray_detector
 	set_physics_process(true)
 
 func _on_body_exit(body: Node3D):
 	if not body is Player:
 		return
+	raycast.target_position = Vector3(0, 1, 0)
+	raycast = null
 	player = null
 	set_physics_process(false)	
 	
